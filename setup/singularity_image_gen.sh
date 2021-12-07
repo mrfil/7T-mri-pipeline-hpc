d795912160564c0e4b4c606baf63b26dddf6923f
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2021-12-07 14:13:41 -0600

    Update singularity_image_gen.sh

--- setup/singularity_image_gen.sh
+++ setup/singularity_image_gen.sh
@@ -23,6 +23,7 @@ sudo SINGULARITY_NOHTTPS=1 singularity build ubuntu-jq-0.1.sif defjq
 sudo SINGULARITY_NOHTTPS=1 singularity build python3.sif defpy3
 sudo SINGULARITY_NOHTTPS=1 singularity build laynii-2.0.0.sif layniidef
 sudo SINGULARITY_NOHTTPS=1 singularity build ashs-1.0.0.sif ashsdef
+sudo SINGULARITY_NOHTTPS=1 singularity build pylearn.sif pylearndef
 
 #Start Docker registry for localhost
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
