c04c3b7e32051929b571484789e558ffcc1f94b2
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2021-12-08 11:22:06 -0600

    Add files via upload

---
+++ noddi_stats.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# mount qsirecon/subject/session/dwi to /datanoddi
+
+subject=$1
+sesname=$2
+cd /shared/mrfil-data/pcamach2/CUPS/bids/derivatives/qsirecon/sub-CUPS005/ses-A/dwi
+# cd /datanoddi
+for atlas in aal116 aicha384 brainnetome246 gordon333 power264 schaefer100x17 schaefer100x7 schaefer200x17 schaefer200x7 schaefer400x17 schaefer400x7; do
+    roi_names="./*"${atlas}"_mrtrixLUT.txt"
+    parc_file="./*${atlas}_atlas.nii.gz"
+    echo $roi_names
+    echo "Creating temporary masks for each ROI based on $atlas parcellation"
+    mkdir ./masks
+    while read roi_name_tmp; do
+        roi_num_tmp=`echo $roi_name_tmp | sed 's@^[^0-9]*\([0-9]\+\).*@\1@'`
+        roi_name=`cut -d ' ' -f 2 <<< $roi_name_tmp`
+        roi_lowthresh=$( echo "scale=2; $roi_num_tmp - 0.5" | bc )
+        roi_highthresh=$( echo "scale=2; $roi_num_tmp + 0.5" | bc )
+        echo "$roi_name_tmp"
+        echo "$roi_num_tmp"
+        echo "$roi_lowthresh"
+        echo "$roi_name"
+        touch roi_csv_tmp.csv
+        fslmaths ${parc_file} -thr $roi_lowthresh -uthr $roi_highthresh roitmp.nii.gz
+        for metric in ICVF ISOVF OD; do
+            echo "Calculating ROI stats in $metric image"
+            metricImage="./*${metric}_NODDI.nii.gz"
+            fslstats ${metricImage} -k roitmp.nii.gz -M -S -R -r > roistats.txt
+            roi_stats_tmp=`cat roistats.txt`
+            echo $roi_stats_tmp
+            echo "${metric}_${atlas}_${roi_name}_mean_nonzero ${metric}_${atlas}_${roi_name}_standard_deviation ${metric}_${atlas}_${roi_name}_min ${metric}_${atlas}_${roi_name}_max ${metric}_${atlas}_${roi_name}_min_robust ${metric}_${atlas}_${roi_name}_max_robust" > roistats.csv
+            echo $roi_stats_tmp >> roistats.csv
+            sed -i 's/ *$//' roistats.csv
+            sed -i 's/\ /,/g' roistats.csv
+            # mv -d, roi_csv_tmp.csv roistats.csv > ROI_STATS.csv
+            # cp ROI_STATS.csv roi_csv_tmp.csv
+            sed -i 's/\([^,]*\),\(.*\)/\2/' roistats.csv
+            mv roistats.csv ${subject}_${sesname}_${metric}_${atlas}_${roi_name}_roistats.csv
+            echo "Cleaning tmp files"
+            rm roistats.txt
+        done
+        mv roitmp.nii.gz ./masks/${roi_name}.nii.gz
+        rm roi_csv_tmp.csv
+    done <$roi_names
+    rm -rf ./masks
+    paste -d, ${subject}_${sesname}_ICVF_${atlas}_*_roistats.csv > ${subject}_${sesname}_ICVF_${atlas}.csv
+    paste -d, ${subject}_${sesname}_ISOVF_${atlas}_*_roistats.csv > ${subject}_${sesname}_ISOVF_${atlas}.csv
+    paste -d, ${subject}_${sesname}_OD_${atlas}_*_roistats.csv > ${subject}_${sesname}_OD_${atlas}.csv
+    echo "Statistics available in QSIPrep directory"
+done
+echo "NODDI stats finished"
+rm *_roistats.csv
+exit 0
---
+++ slurm_orig_qsm_stats_rage.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#slurm_process_pipeline.sh
+
+while getopts :p:s:z:m:f:l:b:t: option; do
+	case ${option} in
+    	p) export CLEANPROJECT=$OPTARG ;;
+    	s) export CLEANSESSION=$OPTARG ;;
+    	z) export CLEANSUBJECT=$OPTARG ;;
+	m) export MINQC=$OPTARG ;;
+	f) export fieldmaps=$OPTARG ;;
+	l) export longitudinal=$OPTARG ;;
+	b) export based=$OPTARG ;;
+	t) export version=$OPTARG ;;
+	esac
+done
+## takes project, subject, and session as inputs
+
+pilotdir=${based}/original_location_of_images_from_XNAT
+IMAGEDIR=${based}/singularity_images
+tmpdir=${based}/${version}/testing
+scripts=${based}/${version}/scripts
+bids_out=${based}/${version}/bids_only
+conn_out=${based}/${version}/conn_out
+dataqc=${based}/${version}/data_qc
+stmpdir=${based}/${version}/scratch/stmp
+scachedir=${based}/${version}/scratch/scache
+
+cd $pilotdir
+
+DIR=${CLEANPROJECT}/${CLEANSUBJECT}/${CLEANSESSION}
+
+
+## setup our variables and change to the session directory
+
+echo ${CLEANPROJECT}
+echo ${CLEANSUBJECT}
+echo ${CLEANSESSION}
+pwd
+
+#translating naming conventions
+echo "${CLEANSESSION: -1}"
+session="${CLEANSESSION: -1}"
+echo ${session}
+project=${CLEANPROJECT}
+mkdir -p ${tmpdir}/${project}/${CLEANSUBJECT}/${session}
+cp -R ${pilotdir}/${DIR} ${tmpdir}/${project}/${CLEANSUBJECT}/${session}
+
+subject="sub-"${CLEANSUBJECT}
+sesname="ses-"${session}
+
+	projDir=${tmpdir}/${project}
+	scripts=${based}/${version}/scripts
+
+	cd $projDir
+
+	IMAGEDIR=${based}/singularity_images
+	CACHESING=${scachedir}/${project}_${subject}_${sesname}_dcm2rsfc
+	TMPSING=${stmpdir}/${project}_${subject}_${sesname}_dcm2rsfc
+	mkdir $CACHESING
+	mkdir $TMPSING
+
+	ses=${sesname:4}
+	sub=${subject:4}
+
+	cd ${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out
+	roi_names=$scripts/aparc_cort_subcort_labels.txt
+	acqtag="_acq-mp2rageunidenoised_"
+	
+	echo "afni deoblique and resample fmriprep anat outputs"
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/fmriprep/${subject}/${sesname}/anat:/datafs,${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif 3dWarp -oblique2card -prefix /dataqsm/card_"$subject"_"$sesname""$acqtag"desc-brain_mask.nii.gz /datafs/"$subject"_"$sesname""$acqtag"desc-brain_mask.nii.gz
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/fmriprep/${subject}/${sesname}/anat:/datafs,${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif 3dresample -dxyz 1 1 1 -prefix /dataqsm/resample_card_"$subject"_"$sesname""$acqtag"desc-brain_mask.nii.gz -input /dataqsm/card_"$subject"_"$sesname""$acqtag"desc-brain_mask.nii.gz
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/fmriprep/${subject}/${sesname}/anat:/datafs,${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif 3dWarp -oblique2card -prefix /dataqsm/card_"$subject"_"$sesname""$acqtag"desc-preproc_T1w.nii.gz /datafs/"$subject"_"$sesname""$acqtag"desc-preproc_T1w.nii.gz
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/fmriprep/${subject}/${sesname}/anat:/datafs,${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif 3dresample -dxyz 1 1 1 -prefix /dataqsm/resample_card_"$subject"_"$sesname""$acqtag"desc-preproc_T1w.nii.gz -input /dataqsm/card_"$subject"_"$sesname""$acqtag"desc-preproc_T1w.nii.gz
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/fmriprep/${subject}/${sesname}/anat:/datafs,${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif 3dWarp -oblique2card -prefix /dataqsm/card_"$subject"_"$sesname""$acqtag"desc-aparcaseg_dseg.nii.gz /datafs/"$subject"_"$sesname""$acqtag"desc-aparcaseg_dseg.nii.gz
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/fmriprep/${subject}/${sesname}/anat:/datafs,${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif 3dresample -dxyz 1 1 1 -prefix /dataqsm/resample_card_"$subject"_"$sesname""$acqtag"desc-aparcaseg_dseg.nii.gz -input /dataqsm/card_"$subject"_"$sesname""$acqtag"desc-aparcaseg_dseg.nii.gz
+	
+	echo "Intensity Non-Uniformity Correction with FSL FAST"
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif fast -B -b -t 2 /dataqsm/${subject}_${sesname}_ndi_mag_fp2.nii
+	#flirt transform routine
+       	#extract brain from fmriprep
+        echo "Extracting brain from T1w based on fMRIPrep ANTs output"
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif fslmaths /dataqsm/resample_card_"$subject"_"$sesname""$acqtag"desc-preproc_T1w.nii.gz -mas /dataqsm/resample_card_"$subject"_"$sesname""$acqtag"desc-brain_mask.nii.gz /dataqsm/resample_card_"$subject"_"$sesname""$acqtag"desc-brain.nii.gz
+	echo "Registering swi magnitude image to resampled deobliqued preprocessed T1w (see fmriprep anat outputs for more details)"
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif flirt -cost normmi -dof 12 -in /dataqsm/resample_card_"$subject"_"$sesname""$acqtag"desc-brain.nii.gz -ref /dataqsm/${subject}_${sesname}_ndi_mag_fp2_restore.nii.gz -omat /dataqsm/rage2swi.mat -out /dataqsm/rage_in_mag.nii.gz	
+	echo "Inverting transform matrix"
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif convert_xfm -omat /dataqsm/swi2rage.mat -inverse /dataqsm/rage2swi.mat
+	echo "Registering resampled deobliqued freesurfer parcellation to swi magnitude image (see fmriprep anat outputs for more details)"
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm $IMAGEDIR/neurodoc.sif flirt -interp sinc -in /dataqsm/"$subject"_"$sesname"_ndi_qsm_fp2 -ref /dataqsm/resample_card_"$subject"_"$sesname""$acqtag"desc-brain.nii.gz -applyxfm -init /dataqsm/swi2rage.mat -out /dataqsm/QSM_to_RAGE.nii.gz
+	echo "Calculation ROI-wise stats on QSM image using fslstats"
+	SINGULARITY_CACHEDIR=$CACHESING SINGULARITY_TMPDIR=$TMPSING singularity exec --cleanenv --bind ${projDir}/bids/derivatives/swi_old/${subject}/${sesname}/ndi_out:/dataqsm,${scripts}:/scripts $IMAGEDIR/neurodoc.sif /scripts/qsm_stats_rage.sh ${subject} ${sesname}
